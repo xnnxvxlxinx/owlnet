f40ab9d64fd316cb606c64e31672182ed79ec33b
xnnxvxlxinx <32514087+xnnxvxlxinx@users.noreply.github.com>
2019-07-02 16:59:26 -0500

    2

--- platforms/browser/www/js/conexionDomino.js
+++
@@ -1,6 +0,0 @@
-
-function obtenerURL(){
-	var url='http://55af0de3.ngrok.io';
-	//var url='http://localhost';
-	return url;
-}
--- www/js/conexionDomino.js
+++
@@ -1,6 +0,0 @@
-
-function obtenerURL(){
-	var url='http://ef07f890.ngrok.io';
-	//var url='http://localhost';
-	return url;
-}
